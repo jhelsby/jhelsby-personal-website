fad21258771e6f20af0cc47f84db5140189127f9
jlh <11036537+jhels@users.noreply.github.com>
2024-04-07 20:27:56 +0100

    Fixed incomplete colouring issue by extending body to bottom of page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,9 @@
+'use client'
+
 import React from 'react';
 import { Sidebar, SidebarToggle } from '../Sidebar';
 
 export default function About() {
-
   return (
     <div id="outer-container">
       <Sidebar />
@@ -11,15 +12,13 @@ export default function About() {
         <p>I&apos;m John Helsby, a backend software engineer and a second-year Computer Science & Mathematics with Industrial Placement Year BSc (Hons) student at the University of Bath, United Kingdom.</p>
         <p>You can view some of the personal projects I have worked on <a href="/#projects">here</a>, check out my <a href="/blog">Technical Blog</a>, or read about my professional and academic experience below.</p>
         <div>
-            <h2>Experience</h2>
-            <p>Wolfram.</p>
+          <h2>Experience</h2>
+          <p>Wolfram.</p>
         </div>
         <div>
-            <h2>Education</h2>
-            <p>University of Bath.</p>
-
+          <h2>Education</h2>
+          <p>University of Bath.</p>
         </div>
-
       </div>
     </div>
   );
