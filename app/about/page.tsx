470199bec8da788b1606df02684506fba996e38c
jhels <11036537+jhels@users.noreply.github.com>
2024-04-09 21:01:59 +0100

    About Me formatting and structure

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,58 @@
 'use client'
 
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Sidebar, SidebarToggle } from '../Sidebar';
+
+// Define an interface for Experience data
+interface Entry {
+  title: string;
+  company: string;
+  start: string;
+  end: string;
+  description: ReactNode;
+}
+
+interface SectionProps {
+  entries: Entry[];
+  sectionTitle: string;
+}
+
+// Sample experience data
+const experiences: Entry[] = [
+  {
+    title: "Compiler Developer Intern",
+    company: "Wolfram Research, Oxfordshire",
+    start: "June 2023",
+    end: "September 2023",
+    description: <div><p>Test</p></div>,
+  },
+  // Add more experiences as needed
+];
+
+const education: Entry[] = [
+  {
+    title: "Computer Science & Mathematics BSc (Hons)",
+    company: "University of Bath",
+    start: "September 2022",
+    end: "June 2026",
+    description: <div><p>Course includes year long industrial placement.</p></div>
+  },
+  // Add more education entries as needed
+];
+
+const Section: React.FC<SectionProps> = ({ entries, sectionTitle }) => {
+  return (
+    <div>
+      <h2>{sectionTitle}</h2>
+      {entries.map((entry, index) => (
+        <div key={index} style={{ marginBottom: '2rem' }}>
+          <p><strong>{entry.title}, {entry.company}.</strong><br />{entry.start} &ndash; {entry.end}</p>
+          {entry.description}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default function About() {
   return (
@@ -10,16 +61,11 @@ export default function About() {
       <div id="page-wrap">
         <h1> <SidebarToggle /> About Me.</h1>
         <p>I&apos;m John Helsby, a backend software engineer and a second-year Computer Science & Mathematics with Industrial Placement Year BSc (Hons) student at the University of Bath, United Kingdom.</p>
-        <p>You can view some of the personal projects I have worked on <a href="/#projects">here</a>, check out my <a href="/blog">Technical Blog</a>, or read about my professional and academic experience below.</p>
-        <div>
-          <h2>Experience</h2>
-          <p>Wolfram.</p>
-        </div>
-        <div>
-          <h2>Education</h2>
-          <p>University of Bath.</p>
-        </div>
+        <p>You can view my professional and academic experience below, or see my <a href="https://www.linkedin.com/in/john-h-366980236/">LinkedIn page</a> for more information.</p>
+
+        <Section entries={experiences} sectionTitle="Experience" />
+        <Section entries={education} sectionTitle="Education" />
       </div>
-    </div>
+    </div >
   );
 }
