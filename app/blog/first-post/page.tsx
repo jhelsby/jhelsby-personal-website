1c263a77fe8cd37d4f4f340db6acb050d0da65e7
jhels <11036537+jhels@users.noreply.github.com>
2024-04-10 13:43:04 +0100

    Wording

--- app/blog/first-post/page.tsx
+++ app/blog/first-post/page.tsx
@@ -8,7 +8,7 @@ export default function Post() {
     <div id="outer-container">
       <Sidebar />
       <div id="page-wrap">
-        <h1><SidebarToggle /> First Post - Introduction and Motivation</h1>
+        <h1><SidebarToggle /> First Post - Introduction and Motivation.</h1>
         <p><a href="/blog">Back to blog homepage</a>.</p>
         <p>
           Introduction.
