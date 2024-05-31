abf4d2f8d0d364384fd9741afd2e1005062a58a1
jhels <11036537+jhels@users.noreply.github.com>
2024-05-31 16:07:25 +0100

    Typo

--- app/blog/dependencies/page.tsx
+++ app/blog/dependencies/page.tsx
@@ -12,7 +12,7 @@ const body = <div>
   <p>
     When trying to solving a problem, I often begin by trying to see if it has already been solved by someone else. This might be anything from a complete product, to a library, to a line or two of code. Provided that the goal is to solve the problem, rather than train one&apos;s problem solving abilities, I think this approach can make a lot of sense &ndash; why reinvent the wheel without good reason?
   </p>
-  <p>The trouble is when this solution you are now dependent on _doesn&apos;t work_.</p>
+  <p>The trouble is when this solution you are now dependent on <i>doesn&apos;t work</i>.</p>
   <p>[TBC.]</p>
 </div>
 
