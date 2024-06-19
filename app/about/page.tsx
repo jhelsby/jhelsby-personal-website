9a65c1826966afe4427959c9eb8e19137b0d27f3
jhels <11036537+jhels@users.noreply.github.com>
2024-06-19 23:21:13 +0100

    Amazon info

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -61,7 +61,9 @@ const experiences: Entry[] = [
     company: "Amazon, London",
     start: "June 2024",
     end: "November 2024",
-    description: <p>I am currently undertaking a six month Software Development Engineering placement with Amazon, at their <a href="https://www.aboutamazon.co.uk/news/working-at-amazon/amazon-office-london-photo-tour">1 Principal Place</a> office in London.</p>
+    description: <div><p>I am currently undertaking a six month Software Development Engineering placement with Amazon, at their <a href="https://www.aboutamazon.co.uk/news/working-at-amazon/amazon-office-london-photo-tour">1 Principal Place</a> office in London.</p>
+      <p>I am working in the Rufus Voice team, on Amazon's mobile app. <a href="https://www.aboutamazon.com/news/retail/amazon-rufus">Rufus</a> is an LLM chatbot which provides a new, generative AI-powered, conversational shopping experience.</p>
+    </div>
   },
   {
     title: "Compiler Developer Intern",
