5d2d93d1953627e151237ca345b3aefc0ee93877
jhelsby <11036537+jhelsby@users.noreply.github.com>
2025-02-05 04:24:28 +0000

    Update page.tsx

--- app/blog/dependencies/page.tsx
+++ app/blog/dependencies/page.tsx
@@ -13,12 +13,12 @@ const body = <div>
   <p>
     Now &ndash; each time you do this, you are depending on someone else to solve your problem. There are two critical questions which I think must be asked each time you do this:
   </p>
-  <ol>
+  <ul>
     <li>Is this code really solving the <i>your</i> problem?</li>
     <li>Does this code really work?</li>
-  </ol>
+  </ul>
   <p>Someone else wrote this code. If they were working on the exact same problem as you, then great &ndash; you just need to make sure they got it right (though this is easier said than done).</p>
-  <p>At other times, though, they will have been solving a slightly different problem. Maybe theirs was more general, or less, or intended for different hardware, or different users, or a smaller userbase, or a much bigger userbase, or needed to be more robust, or didn&apos;t need to be robust at all, or needed to work for the next 50 years, or didn&apos;t need to work for more than an hour, or was designed to be updated every couple of days, or was built on top of ancient and vulnerable legacy software, or... And in all of these cases, of course, it could <i>also</i> be ridden with bugs!</p>
+  <p>At other times, though, they will have been solving a slightly different problem. It could differ in intended generality, hardware, userbase, robustness, longevity, maintainability, or more. It could have a huge dependency graph if its own. It could be buggy or broken.</p>
   <p>There is much more to say about this on the technical side, but I&apos;m not sure that I&apos;m the best person to say it. Here are some experienced engineers discussing the same issue. I&apos;ll update this list if I find anything new.</p>
   <ol>
     <li>Thorsten Ball - <a href="https://thorstenball.com/blog/2022/05/17/professional-programming-the-first-10-years/#using-other-peoples-code-is-not-as-good-as-i-thought">Using other people&apos;s code is not as good as I thought</a> (2022).</li>
