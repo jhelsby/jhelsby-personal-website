b6dc8df78c1225bb47c48148cb637c32e3eca828
jhelsby <11036537+jhelsby@users.noreply.github.com>
2025-02-05 04:30:28 +0000

    Update page.tsx

--- app/blog/dependencies/page.tsx
+++ app/blog/dependencies/page.tsx
@@ -20,12 +20,12 @@ const body = <div>
   <p>Someone else wrote this code. If they were working on the exact same problem as you, then great &ndash; you just need to make sure they got it right (though this is easier said than done).</p>
   <p>At other times, though, they will have been solving a slightly different problem. It could differ in intended generality, hardware, userbase, robustness, longevity, maintainability, or more. It could have a huge dependency graph if its own. It could be buggy or broken.</p>
   <p>There is much more to say about this on the technical side, but I&apos;m not sure that I&apos;m the best person to say it. Here are some experienced engineers discussing the same issue. I&apos;ll update this list if I find anything new.</p>
-  <ol>
+  <ul>
     <li>Thorsten Ball - <a href="https://thorstenball.com/blog/2022/05/17/professional-programming-the-first-10-years/#using-other-peoples-code-is-not-as-good-as-i-thought">Using other people&apos;s code is not as good as I thought</a> (2022).</li>
     <li>A <a href="https://x.com/mitchellh/status/1795554156150657420">series of tweets</a> by Mitchell Hashimoto, founder of <a href="https://en.wikipedia.org/wiki/HashiCorp">HashiCorp</a> - see the linked tweet and his replies. </li>
     <li>Armin Ronacher - <a href="https://lucumr.pocoo.org/2025/1/24/build-it-yourself/
 ">Build It Yourself</a> (2025).</li>
-  </ol>
+  </ul>
   <h3>Deferred costs: the illusion of ease</h3>
   <p>One reason it is so tempting to introduce a dependency, I think, is simply because it is easy. With open-source software, all you need is a <code>git pull</code> and an <code>import</code>; and without having to spend an ounce of effort or money, you have tens, hundreds, or even thousands of hours&apos; worth of someone else&apos;s code working for you. The problem is... where did that code come from? What was it for? How well has it been tested?</p>
   <p>When there is a substantial cost to getting hold of something, whether time, money, energy, or anything else, it forces you to think about your decision <i>before</i> you act. You want it to be worth the price. The bigger the cost, the more you&apos;d want to think about it.</p>
