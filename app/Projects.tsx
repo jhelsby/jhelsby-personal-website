9ba27aa3ff12f2e737cc0b67491e5fa2ef9932ff
jhels <11036537+jhels@users.noreply.github.com>
2025-04-19 11:45:40 +0200

    wording

--- app/Projects.tsx
+++ app/Projects.tsx
@@ -127,7 +127,7 @@ const Conduit: ProjectFunction = () => {
 const LoxInterpreters: ProjectFunction = () => {
   return {
     summary: <p>
-      A work-in-progress implementation of <i>clox</i>, an interpreter for the Lox programming language written in C. clox comprises of a bytecode compiler and a virtual machine with garbage collection. I have also written <a href="https://github.com/jhelsby/jlox-notes">complete notes on <i>jlox</i></a>, a tree-walk interpreter for the programming language Lox, written in Java.
+      A work-in-progress implementation of <i>clox</i>, an interpreter for the Lox programming language written in C. clox comprises of a bytecode compiler and a virtual machine with garbage collection. I have also written <a href="https://github.com/jhelsby/jlox-notes">complete notes on <i>jlox</i></a>, a tree-walk interpreter for Lox written in Java.
     </p>,
     description: <p>
       <i>clox</i>, <i>jlox</i> and Lox are described in <a href="https://craftinginterpreters.com/">Crafting Interpreters</a> (2021), by Robert Nystrom. Lox is a simple, dynamically typed, object-oriented language.
